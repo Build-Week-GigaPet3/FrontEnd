758bae023c96db185c334d9971b8f80996af4f9e
Alexander C <alexanderlcooter@gmail.com>
2019-12-30 15:29:37 -0600

    Header component built out based on figma wireframe

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import PrivateRoute from './components/PrivateRoute';
 import TestLogin from './components/TestLogin';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Header />
       <div className="App">
       <header className="App-header">
         <Route exact path='/' component={TestLogin} />
