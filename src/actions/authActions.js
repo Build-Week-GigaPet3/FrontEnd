8cced8063c6b14b0d467d98c60131f010420693b
Robert Gordon <Komeuppance@gmail.com>
2020-01-08 09:38:50 -0800

    Missing token fixed

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -8,6 +8,26 @@ const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const LOGIN_FAIL = 'LOGIN_FAIL';
 const LOGOUT = 'LOGOUT';
 
+// const registerUser = (values, redirect) => dispatch => {
+//     dispatch({type: REGISTRATION_REQUEST});
+
+//     axiosWithAuth()
+//         .post('/auth/register', values)
+//         .then(res => {
+//             console.log ('register user', res.data)
+//             const { token } = res.data;
+//             const data = {
+//                 id: res.data.id,
+//                 username: res.data.username,
+//             }
+//             sessionStorage.setItem('token', token);
+//             sessionStorage.setItem('user', JSON.stringify(data.id));
+//             dispatch({type: REGISTRATION_SUCCESS, payload: data });
+//             redirect();
+//         })
+//         .catch(err => dispatch({type: REGISTRATION_FAIL, payload: err.message}));
+// }
+
 const registerUser = (values, redirect) => dispatch => {
     dispatch({type: REGISTRATION_REQUEST});
 
@@ -15,15 +35,30 @@ const registerUser = (values, redirect) => dispatch => {
         .post('/auth/register', values)
         .then(res => {
             console.log ('register user', res.data)
-            const { token } = res.data;
-            const data = {
-                id: res.data.id,
-                username: res.data.username,
+            if (res.data.username) {
+                const missingTokenLogin = {
+                    username: values.username,
+                    password: values.password
+                }
+                axiosWithAuth()
+                    .post('/auth/login', missingTokenLogin)
+                    .then(res => {
+                        const { token } = res.data;
+                        console.log('token fix user logged in', res.data);
+                        const data = {
+                            id: res.data.id,
+                            username: res.data.username,
+                        };
+                        sessionStorage.setItem('token', token);
+                        sessionStorage.setItem('user', JSON.stringify(data.id));
+                        sessionStorage.setItem('username', (data.username));
+                        redirect();
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        dispatch({type: LOGIN_FAIL, payload: err.message})
+                    })
             }
-            sessionStorage.setItem('token', token);
-            sessionStorage.setItem('user', JSON.stringify(data.id));
-            dispatch({type: REGISTRATION_SUCCESS, payload: data });
-            redirect();
         })
         .catch(err => dispatch({type: REGISTRATION_FAIL, payload: err.message}));
 }
