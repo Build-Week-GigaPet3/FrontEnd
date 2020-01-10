973bf2c32ca6ac5987a5d72ab4446cc563423aaa
Robert Gordon <Komeuppance@gmail.com>
2020-01-09 19:15:20 -0800

    Height adjust

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ import styled from 'styled-components';
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  /* align-items: center; */
+  /* justify-content: center; */
   background-image: url('../img/rainbowbg.gif');
   background-repeat: no-repeat;
   background-size: contain;
