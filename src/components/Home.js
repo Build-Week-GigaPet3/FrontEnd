80d8a7838b10caf410efe17110b913e5669880db
Robert Gordon <Komeuppance@gmail.com>
2020-01-09 21:48:41 -0800

    Home spam

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,6 +39,42 @@ const Login = (props) =>{
                 <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
             </div>
             <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
+            <h2>What it does</h2>
+            <div className='text'>
+                <p>Being a parent we know you are always looking for ways to motivate your kids to eat. With this app, you can input the servings of food you child eats, and the child can watch their Lambdi Pet grow and flourish, or wither and struggle based on what food they eat.</p>
+                <p>Lambdi Pet gives your child control over feeding their little buddy and are able to see what happends when they make healthy eating choices.</p>
+            </div>
+            <a href='/signup' ><Button name="Create Account" /></a>
         </Container>
     )
 }
